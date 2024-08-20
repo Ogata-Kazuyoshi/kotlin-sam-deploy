55d70593e692db59c64246ba1b856bdbab1f75dd
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-08-20 18:51:38 +0900

    AWSにPushトライ6

--- kotlin-lambda/HelloWorldFunction/src/test/kotlin/helloworld/AppTest.kt
+++ kotlin-lambda/HelloWorldFunction/src/test/kotlin/helloworld/AppTest.kt
@@ -9,16 +9,16 @@ import org.mockito.Mockito.mock
 class AppTest {
     @Test
     fun successfulResponse() {
-//        val app = App()
-//        val input = APIGatewayProxyRequestEvent()
-//        val context = mock(Context::class.java)
-//        val result = app.handleRequest(input, context)
-//        assertEquals(200, result.statusCode)
-//        assertEquals("application/json", result.headers["Content-Type"])
-//        val content = result.body
-//        assertNotNull(content)
-//        assertTrue(content.contains("\"message\""))
-//        assertTrue(content.contains("\"hello world\""))
-//        assertTrue(content.contains("\"location\""))
+        val app = App()
+        val input = APIGatewayProxyRequestEvent()
+        val context = mock(Context::class.java)
+        val result = app.handleRequest(input, context)
+        assertEquals(200, result.statusCode)
+        assertEquals("application/json", result.headers["Content-Type"])
+        val content = result.body
+        assertNotNull(content)
+        assertTrue(content.contains("\"message\""))
+        assertTrue(content.contains("\"hello world\""))
+        assertTrue(content.contains("\"location\""))
     }
 }
